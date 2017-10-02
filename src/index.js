4007d363bd39aad3e18f8ca3a5970d1742be7f85
Victor Viale <victor.viale@wanadoo.fr>
2017-10-02 11:34:29 +0000

    Cleaned day1 to get a template for later

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import 'aframe-curve-component';
 import 'babel-polyfill';
 
 import {Entity, Scene} from 'aframe-react';
-import BirdManager from './BirdManager';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -14,22 +13,16 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   render () {
     return (
-      <Scene antialias="true" inspector="url: https://aframe.io/releases/0.3.0/aframe-inspector.min.js">
+      <Scene gridhelper="colorGrid: #00FF00" antialias="true" inspector="url: https://aframe.io/releases/0.3.0/aframe-inspector.min.js">
         <a-assets>
 
         </a-assets>
         
-        <BirdManager interval="500" />
+
 
         <Entity primitive="a-sky" color="black" />
 
-        <Entity primitive="a-camera" position="0 -5 0" rotation="90 90 0">
-          <Entity primitive="a-light" 
-          type="point"
-          color="#FFFFFF"
-          intensity="1000"
-          distance="10"/>
-        </Entity>
+        <Entity primitive="a-camera" position="0 -5 0"  />
       </Scene>
     );
   }
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,14 @@ export function uid(len) {
   len = len || 7;
   return Math.random().toString(35).substr(2, len);
 }
+
+export function rand(min, max) {
+  return Math.random() * (max - min) + min;
+}
+  
+export function randWithGap(min, max) {
+  const val = Math.random() * (max - min) + min;
+  
+  return (val < 0 ? val - 1 : val + 1)
+}
+  
