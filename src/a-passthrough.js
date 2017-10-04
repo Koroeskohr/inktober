076b617a416ab3090d62175241559cfcf8540e69
Victor Viale <victor.viale@wanadoo.fr>
2017-10-04 20:19:35 +0000

    Day 4 : underwater

---
+++ src/a-passthrough.js
@@ -0,0 +1,27 @@
+/* global AFRAME  */
+
+var extendDeep = AFRAME.utils.extendDeep;
+var meshMixin = AFRAME.primitives.getMeshMixin();
+
+AFRAME.registerPrimitive('a-passthrough', extendDeep({}, meshMixin, {
+  defaultComponents: {
+    passthrough: {},
+    position: "0 0 -20",
+    geometry: {
+      primitive: 'plane'
+    },
+    material: {
+      color: '#FFF',
+      shader: 'flat',
+      side: 'double',
+      transparent: true,
+      width: 16,
+      height: 9
+    }
+  },
+
+  mappings: {
+    height: 'geometry.height',
+    width: 'geometry.width'
+  }
+}));
--- src/index.js
+++ src/index.js
@@ -3,26 +3,58 @@ import 'aframe-gridhelper-component';
 import 'aframe-animation-component';
 import 'aframe-alongpath-component';
 import 'aframe-curve-component';
+import './passthrough'
+
 import 'babel-polyfill';
+
+import {rand} from './utils'
 
 import {Entity, Scene} from 'aframe-react';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function Bubble(props) {
+  const x = props.x
+  return (
+    <a-sphere position={`${x} -1 -2`} scale="0.03 0.03 0.03" material="color: #3333CC;" >
+      <a-animation 
+        begin={props.delay.toString()}
+        attribute="position"
+        easing="ease-sine"
+        dur="4000"
+        to={`${x} 4 -2`}
+        repeat="indefinite"></a-animation>
+    </a-sphere>
+  )
+}
+
+
+
+let xs = []
+
+for (let i= -3; i<=3; i+=rand(0.2, 0.6)) {
+  xs.push({x: i, delay: Math.round(rand(0, 4000))})
+}
+
 class App extends React.Component {
   render () {
     return (
-      <Scene gridhelper="colorGrid: #00FF00" antialias="true" inspector="url: https://aframe.io/releases/0.3.0/aframe-inspector.min.js">
+      <Scene inspector="url: https://aframe.io/releases/0.3.0/aframe-inspector.min.js">
         <a-assets>
-
         </a-assets>
+        {
+          xs.map(x => 
+            <Bubble x={x.x} delay={x.delay} />
+          )
+        }
         
-
-
         <Entity primitive="a-sky" color="black" />
 
-        <Entity primitive="a-camera" position="0 -5 0"  />
+        <Entity primitive="a-camera" position="0 0 0" >
+          <a-passthrough width="16" height="12" position="0 0 -4"/>
+          <a-plane position="1 0 -0.1" material="color: #6666CC; transparent: true; opacity: 0.7" scale="20 20 1" />
+        </Entity>
       </Scene>
     );
   }
